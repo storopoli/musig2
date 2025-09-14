7c6fff7a2d23f4665cc8ad90544eeee5373d7fec
Jose Storopoli <jose@storopoli.com>
2025-09-14 18:16:05 -0300

    feat: KeyAggContext

--- lib/Crypto/Curve/Secp256k1/MuSig2.hs
+++ lib/Crypto/Curve/Secp256k1/MuSig2.hs
@@ -14,6 +14,10 @@ MuSig2 signing Haskell library.
 TODO: add description
 -}
 module Crypto.Curve.Secp256k1.MuSig2 (
+  -- main interfaces
+  KeyAggContext (..),
+  mkKeyAggContext,
+  Tweak (..),
   -- Pubkey functions
   sortPubkeys,
   aggPubkeys,
@@ -25,13 +29,77 @@ module Crypto.Curve.Secp256k1.MuSig2 (
 )
 where
 
-import Crypto.Curve.Secp256k1 (Projective, Pub, add, modQ, mul, serialize_point, _CURVE_ZERO)
+import Crypto.Curve.Secp256k1 (Projective, Pub, add, modQ, mul, serialize_point, _CURVE_Q, _CURVE_ZERO)
 import Crypto.Hash.SHA256 (hash)
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
 import Data.List (find, sort)
 import Data.Maybe (fromMaybe)
+import Data.Word (Word32)
 import GHC.List (foldl')
+
+-- | Key aggregation context that holds the aggregated public key and a tweak, if applicable.
+data KeyAggContext = KeyAggContext
+  { q :: Projective
+  -- ^ Point representing the potentially tweaked aggregate public key: an elliptic curve point.
+  , publicKeys :: [Pub]
+  -- ^ Ordered |List| of |Pub|keys.
+  , coefficients :: [Integer]
+  -- ^ |List| of aggregation coefficients.
+  , tacc :: Maybe Tweak
+  -- ^ accumulated tweak: an integer with \(0 \leq tacc < n\) where \(n\) is the curve order. |Nothing| means \(0\).
+  , gacc :: Bool
+  -- ^ parity accumulator: |False| means \(g = 1\), |True| means \(g = n-1\) where \(n\) is the curve order.
+  }
+
+{- | Creates a 'KeyAggContext' from a 'List' of 'Pub'keys.
+
+The order in which the 'Pub'keys are presented will be preserved.
+A specific ordering of 'Pub'keys will uniquely determine the aggregated 'Pub'key.
+
+If the same keys are provided again in a different sorting order, a different
+aggregated 'Pub'key will result. It is recommended to sort keys ahead of time
+using 'sortPubkey' before creating a 'KeyAggContext'.
+
+
+== NOTE
+
+Internally it validates if all keys and the resulting aggregated key are not
+points at infinity, if the optional tweak is within the curve order, and if
+the length of the list of keys is not bigger than 32 bits.
+-}
+mkKeyAggContext :: [Pub] -> Maybe Tweak -> KeyAggContext
+mkKeyAggContext pks mTweak
+  | null pks = error "mkKeyAggContext: empty public key list"
+  | length pks > fromIntegral (maxBound :: Word32) = error "mkKeyAggContext: too many public keys (max 2^32 - 1)"
+  | _CURVE_ZERO `elem` pks = error "mkKeyAggContext: public key at point of infinity"
+  | maybe False ((< 0) . getTweak) mTweak = error "mkKeyAggContext: tweak must be non-negative"
+  | maybe False ((>= _CURVE_Q) . getTweak) mTweak = error "mkKeyAggContext: The tweak must be less than n"
+  | otherwise = case aggPubkeys pks of
+      Nothing -> error "mkKeyAggContext: failed to aggregate public keys"
+      Just aggPk
+        | aggPk == _CURVE_ZERO -> error "mkKeyAggContext: aggregated public key is point at infinity"
+        | otherwise ->
+            let coeffs' = map (`computeKeyAggCoef` pks) pks
+                parity = case mTweak of
+                  Just (XOnlyTweak _) -> not (isEvenPub aggPk)
+                  _ -> False
+             in KeyAggContext aggPk pks coeffs' mTweak parity
+
+-- | Tweak that can be added to an aggregated 'Pub'key.
+data Tweak
+  = -- | X-only tweak required by Taproot tweaking to add script paths to a Taproot output.
+    -- See [BIP341](https://github.com/bitcoin/bips/blob/master/bip-0341.mediawiki).
+    XOnlyTweak Integer
+  | -- | Plain tweak that can be used to derive child aggregated 'Pub'keys per
+    -- [BIP32](https://github.com/bitcoin/bips/blob/master/bip-0032.mediawiki)
+    PlainTweak Integer
+  deriving (Read, Show, Eq, Ord)
+
+-- | Retrieves the 'Integer' from 'Tweak'.
+getTweak :: Tweak -> Integer
+getTweak (XOnlyTweak int) = int
+getTweak (PlainTweak int) = int
 
 -- | Manual 'Ord' implementation of 'Projective' for lexicography sorting.
 instance Ord Projective where
@@ -67,6 +135,8 @@ respective public key aggregation coefficient.
 
 'aggPubKeys' do not sort the keys and aggregates public keys according to the
 ordering of the 'List' provided.
+
+You should probably be using the recommended 'mkKeyAggContext'.
 -}
 aggPubkeys :: [Pub] -> Maybe Pub
 aggPubkeys [] = Nothing
